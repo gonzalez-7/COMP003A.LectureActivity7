78b46b6310a5bb9fa46a3f14e00025906628f5e2
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-25 19:24:19 -0800

    Added names list and foreach loop to iterate and display values.

--- COMP003A.LectureActivity7/Program.cs
+++ COMP003A.LectureActivity7/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace COMP003A.LectureActivity7
+﻿using System.Collections.Generic;
+
+namespace COMP003A.LectureActivity7
 {
 	internal class Program
 	{
@@ -11,6 +13,21 @@
 			for (int i = 0; i < scores.Length; i++)
 			{
 				Console.WriteLine(scores[i]);
+			}
+
+			//
+
+			List<string> names = new List<string>();
+			names.Add("Alex");
+			names.Add("Jordan");
+			names.Add("Taylor");
+
+			Console.WriteLine();
+			Console.WriteLine("Names using foreach loop: ");
+
+			foreach (string name in names)
+			{
+				Console.WriteLine(name);
 			}
 		}
 	}
